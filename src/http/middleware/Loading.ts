58291e65fd540a1446e4b8eb2e15f970d5fd0088
Luke Policinski <luke.policinski@egov.com>
2018-01-23 15:47:30 -0500

    Prettying

--- src/http/middleware/Loading.ts
+++ src/http/middleware/Loading.ts
@@ -1,26 +1,24 @@
 export default class Loading {
-
   private timeoutSet = false;
   private spinnerTimeout = null;
   private requestsCounter = 0;
 
-  public request(config : object) {
+  public request(config: object) {
     if (this.timeoutSet === false) {
       this.spinnerTimeout = setTimeout(() => {
-        this._turnOnSpinner()
-      }, 1500)
+        this._turnOnSpinner();
+      }, 1500);
       this.timeoutSet = true;
     }
 
-    this.requestsCounter++
+    this.requestsCounter++;
 
     return config;
   }
 
-  public response(response : object) {
-
+  public response(response: object) {
     if (--this.requestsCounter === 0) {
-      this._turnOffSpinner()
+      this._turnOffSpinner();
     }
 
     return response;
@@ -36,16 +34,16 @@ export default class Loading {
   private _turnOnSpinner() {
     let spinnerElement = this._getSpinnerElement();
 
-    if(!spinnerElement) {
+    if (!spinnerElement) {
       document.body.innerHTML +=
         '<div id="spinner" class="sk-container">' +
-          '<div class="sk-folding-cube">' +
-          '  <div class="sk-cube1 sk-cube"></div>' +
-          '  <div class="sk-cube2 sk-cube"></div>' +
-          '  <div class="sk-cube4 sk-cube"></div>' +
-          '  <div class="sk-cube3 sk-cube"></div>' +
-          '</div>' +
-        '</div>';
+        '<div class="sk-folding-cube">' +
+        '  <div class="sk-cube1 sk-cube"></div>' +
+        '  <div class="sk-cube2 sk-cube"></div>' +
+        '  <div class="sk-cube4 sk-cube"></div>' +
+        '  <div class="sk-cube3 sk-cube"></div>' +
+        "</div>" +
+        "</div>";
     }
   }
 
