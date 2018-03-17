979f5f9dedd276ab0febf9e8a05509a068e5447d
Luke Policinski <Luke@LukePOLO.com>
2018-03-16 22:09:05 -0400

    StyleJS fixes  [skip ci] (#19)
    StyleJS Auto Squash and Merge PR-19

--- src/validation/Validator.ts
+++ src/validation/Validator.ts
@@ -1,11 +1,10 @@
 import Form from "../plugins/forms/Form";
 
 export default class Validator extends Form {
-
   public rules = {};
   public messages = {};
 
   constructor(data: object) {
-      super(data, $app.make('$validator'));
+    super(data, $app.make("$validator"));
   }
 }
