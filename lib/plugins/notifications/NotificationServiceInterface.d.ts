c5bc980345fba0452ef563e704a497b2657753eb
Luke Policinski <luke.policinski@egov.com>
2018-03-17 18:11:26 -0400

    making notifications nullable

--- lib/plugins/notifications/NotificationServiceInterface.d.ts
+++ lib/plugins/notifications/NotificationServiceInterface.d.ts
@@ -1,6 +1,6 @@
 export default interface NotificationServiceInterface {
-    showError(message: string, title: string, duration?: number): void;
-    showInfo(message: string, title: string, duration?: number): void;
-    showSuccess(message: string, title: string, duration?: number): void;
-    showWarning(message: string, title: string, duration?: number): void;
+    showError(message: string, title?: string, duration?: number): void;
+    showInfo(message: string, title?: string, duration?: number): void;
+    showSuccess(message: string, title?: string, duration?: number): void;
+    showWarning(message: string, title?: string, duration?: number): void;
 }
