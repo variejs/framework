885ddafa2fd778ffc8c42047df002fa49679187d
Luke Policinski <luke@lukepolo.com>
2018-09-24 18:36:05 -0400

    removing consoles

--- BaseLayout.vue
+++ BaseLayout.vue
@@ -9,11 +9,7 @@ export default Vue.extend({
   computed: {
     layout() {
       let matched = Object.assign([], this.$route.matched);
-      console.info(matched);
       let route = matched.reverse().find(route => route.meta.layout);
-      console.info(
-        `${typeof route === "object" ? route.meta.layout : "public"}-layout`
-      );
       return `${
         typeof route === "object" ? route.meta.layout : "public"
       }-layout`;
