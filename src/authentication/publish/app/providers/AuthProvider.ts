3d37c371f04fc8d0b70bfffded536d6a7344307c
Luke Policinski <luke@lukepolo.com>
2018-10-25 19:55:28 -0400

    WIP

---
+++ src/authentication/publish/app/providers/AuthProvider.ts
@@ -0,0 +1,8 @@
+import JwtGuard from "varie/lib/authentication/guards/JwtGuard";
+import ServiceProvider from "varie/lib/support/ServiceProvider";
+
+export default class AuthProvider extends ServiceProvider {
+  public register() {
+    this.app.bind("JwtGuard", JwtGuard);
+  }
+}
