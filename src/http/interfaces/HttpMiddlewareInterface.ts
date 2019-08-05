d1c932823e90cc387126c932c2d677bcbb8c9de9
Luke Policinski <luke.policinski@egov.com>
2019-08-05 12:39:22 -0400

    Fixing http middleware interface

--- src/http/interfaces/HttpMiddlewareInterface.ts
+++ src/http/interfaces/HttpMiddlewareInterface.ts
@@ -3,7 +3,11 @@ import HttpResponseInterface from "./HttpResponseInterface";
 import HttpRequestConfigInterface from "./HttpRequestConfigInterface";
 
 export default interface HttpMiddlewareInterface {
-  request(config: HttpRequestConfigInterface): HttpRequestConfigInterface;
-  response(response: HttpResponseInterface): HttpResponseInterface;
+  request(
+    config: HttpRequestConfigInterface,
+  ): HttpRequestConfigInterface | Promise<HttpRequestConfigInterface>;
+  response(
+    response: HttpResponseInterface,
+  ): HttpResponseInterface | Promise<HttpResponseInterface>;
   responseError(responseError: HttpErrorInterface);
 }
